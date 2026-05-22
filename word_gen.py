fb93cc9469a4e1d0b681a8a5a546d80bef6a2baf
Aarush <abtechgen07@gmail.com>
2026-05-22 18:35:51 +0530

    changed word selection to more common words

--- word_gen.py
+++ word_gen.py
@@ -3,5 +3,8 @@ import random
 with open("14855_valid_words.txt") as file:
     words = set(word.strip() for word in file.readlines())
 
-secret = random.choice(list(words))
+with open("possible_answers_list.txt") as file:
+    possible_answers = set(word.strip() for word in file.readlines())
+
+secret = random.choice(list(possible_answers))
 # print(secret) # Testing
